01395c47a45e868b30b82468ef6727eba2efc12c
Andrew <andrewasmit@hotmail.com>
2023-02-14 14:12:11 -0600

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Repertoire</h2>
+        <p>A performance tracker and curricculum planner for Music Teachers</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
