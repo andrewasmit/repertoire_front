841eca14c7c24954cbb26f0ee4e8c9a6cd07f8d7
andrew <andrewasmit@hotmail.com>
2023-02-26 19:39:40 -0600

    created Notification bar for fetches

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Alert, Snackbar } from "@mui/material";
+import Notification from "./Notification";
+
 
 function Home(){
+
+    const [notify, setNotify] = useState({isOpen: false, message: '', type: ''})
+
+    function handleNotify(){
+        setNotify({
+            isOpen: true,
+            message: 'OH YEAH!',
+            type:'success'
+        })
+    }
+
     return(
         <div id="home-page">
             <h1>THIS IS THE HOMEPAGE! YAY!</h1>
+            <Button variant="contained" onClick={handleNotify}>Submit</Button>
+            <Notification notify={notify} setNotify={setNotify}/>
         </div>
     )
 };
---
+++ src/components/Notification.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { Snackbar, Alert } from "@mui/material";
+
+function Notification({ notify, setNotify }){
+
+    function handleClose(e,r){
+        if(r==='clickaway'){
+            return
+        }
+        setNotify({
+            ...notify,
+            isOpen: false
+        })
+    }
+
+    return(
+        <Snackbar
+            open = {notify.isOpen}
+            autoHideDuration={2000}
+            anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+            onClose={handleClose}
+        >
+            <Alert severity={notify.type} onClose={handleClose}>
+                {notify.message}
+            </Alert>
+        </Snackbar>
+    )
+}
+
+
+export default Notification;
